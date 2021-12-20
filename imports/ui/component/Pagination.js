6d9599ca632a5253e35982288a48c71721259ad4
Rumor <rumor.liao@veden.dental>
2021-12-20 11:45:00 +0800

    Pagination style

--- imports/ui/component/Pagination.js
+++ imports/ui/component/Pagination.js
@@ -1,205 +1,198 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
 
 export const Page = ({ currentPage, index, onChange, className }) => (
-  <li>
     <a
-      className={`pagination-link ${className} ${(currentPage === index || (index === 1 && !currentPage)) &&
-        'is-current'}`}
-      aria-label={`Goto page ${index}`}
-      aria-current={index === currentPage && 'page'}
-      onClick={() => onChange(index)}
+        className={`bg-white  text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 text-sm font-medium ${
+            (currentPage === index || (index === 1 && !currentPage)) &&
+            'z-10 bg-gray-50  text-indigo-600 relative text-sm '
+        }`}
+        aria-label={`Goto page ${index}`}
+        aria-current={index === currentPage && 'page'}
+        onClick={() => onChange(index)}
     >
-      {index}
+        {index}
     </a>
-  </li>
 );
 
 Page.propTypes = {
-  currentPage: PropTypes.number.isRequired,
-  index: PropTypes.number.isRequired,
-  className: PropTypes.string,
-  onChange: PropTypes.func.isRequired,
+    currentPage: PropTypes.number.isRequired,
+    index: PropTypes.number.isRequired,
+    className: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
 };
 
 Page.defaultProps = {
-  className: '',
+    className: '',
 };
 
 const Ellipses = () => (
-  <li>
-    <span className="pagination-ellipsis">&hellip;</span>
-  </li>
+    <span className="relative inline-flex items-center px-4 py-2  bg-white text-sm font-medium text-gray-700">...</span>
 );
 
 export const getVisiblePages = (visibleRadius, currentPage, maxPages) => {
-  const visiblePages = [];
+    const visiblePages = [];
 
-  let start = currentPage - visibleRadius;
-  let end = currentPage + visibleRadius;
+    let start = currentPage - visibleRadius;
+    let end = currentPage + visibleRadius;
 
-  if (start < 1) {
-    start = 1;
-    end = start + visibleRadius * 2;
-  }
+    if (start < 1) {
+        start = 1;
+        end = start + visibleRadius * 2;
+    }
 
-  if (end > maxPages) {
-    start = maxPages - visibleRadius * 2;
-    end = maxPages;
-  }
+    if (end > maxPages) {
+        start = maxPages - visibleRadius * 2;
+        end = maxPages;
+    }
 
-  if (visibleRadius * 2 + 1 > maxPages) {
-    start = 1;
-    end = maxPages;
-  }
+    if (visibleRadius * 2 + 1 > maxPages) {
+        start = 1;
+        end = maxPages;
+    }
 
-  for (let i = start; i <= end; i++) {
-    visiblePages.push(i);
-  }
+    for (let i = start; i <= end; i++) {
+        visiblePages.push(i);
+    }
 
-  return visiblePages;
+    return visiblePages;
 };
 
-const Pagination = ({
-  pages,
-  currentPage,
-  visibleRadius,
-  className,
-  isRounded,
-  isSmall,
-  isMedium,
-  isLarge,
-  isCentered,
-  isRight,
-  prevName,
-  nextName,
-  prevClassName,
-  nextClassName,
-  linkClassName,
-  listClassName,
-  onChange,
-  onPerPageChange,
-}) => {
-  const visiblePages = getVisiblePages(visibleRadius, currentPage, pages);
-  const pagesComponents = [];
+const Pagination = ({ pages, currentPage, visibleRadius, onChange, onPerPageChange }) => {
+    const visiblePages = getVisiblePages(visibleRadius, currentPage, pages);
+    const pagesComponents = [];
 
-  const classes = [
-    isRounded && 'is-rounded',
-    isSmall && 'is-small',
-    isMedium && 'is-medium',
-    isLarge && 'is-large',
-    isCentered && 'is-centered',
-    isRight && 'is-right',
-  ].filter(Boolean);
+    if (visiblePages[0] >= 2) {
+        pagesComponents.push(
+            <Page
+                key="page-1"
+                index={1}
+                currentPage={currentPage}
+                onChange={onChange}
+                className="bg-white  text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2  text-sm font-medium"
+            />,
+        );
 
-  if (visiblePages[0] >= 2) {
-    pagesComponents.push(<Page key="page-1" index={1} currentPage={currentPage} onChange={onChange} />);
-
-    if (visiblePages[0] !== 2) {
-      pagesComponents.push(<Ellipses key="ellipses-1" />);
-    }
-  }
-
-  // eslint-disable-next-line max-len
-  visiblePages.map((page) =>
-    pagesComponents.push(<Page key={`page-${page}`} index={page} currentPage={currentPage} onChange={onChange} />),
-  );
-
-  if (currentPage <= pages - visibleRadius - 1 && visiblePages.length < pages) {
-    if (currentPage < pages - visibleRadius - 1) {
-      pagesComponents.push(<Ellipses key="ellipses-2" />);
+        if (visiblePages[0] !== 2) {
+            pagesComponents.push(<Ellipses key="ellipses-1" />);
+        }
     }
 
-    pagesComponents.push(
-      <Page
-        key={`page-${pages}`}
-        className={linkClassName}
-        index={pages}
-        currentPage={currentPage}
-        onChange={onChange}
-      />,
+    // eslint-disable-next-line max-len
+    visiblePages.map((page) =>
+        pagesComponents.push(
+            <Page
+                key={`page-${page}`}
+                index={page}
+                currentPage={currentPage}
+                onChange={onChange}
+                className="relative inline-flex items-center px-4 py-2 bg-white text-sm font-medium text-gray-700"
+            />,
+        ),
     );
-  }
 
-  return (
-    <nav
-      className={`pagination-container pagination ${classes.join(' ')} ${className}`}
-      role="navigation"
-      aria-label="pagination"
-    >
-      <ul className={`pagination-list ${listClassName}`}>
-        <li className="select is-info is-rounded">
-          {/* eslint-disable-next-line radix */}
-          <select defaultValue="10" onChange={(e) => onPerPageChange(parseInt(e.target.value))}>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="25">25</option>
-            <option value="50">50</option>
-            <option value="100">100</option>
-            <option value="1000">1000</option>
-          </select>
-        </li>
-        <li hidden={currentPage === 1}>
-          <a
-            disabled={currentPage === 1}
-            className={`pagination-previous nav ${prevClassName}`}
-            onClick={() => onChange(currentPage - 1)}
-          >
-            {prevName}
-          </a>
-        </li>
-        {pagesComponents}
-        <li hidden={currentPage === pages}>
-          <a
-            disabled={currentPage === pages}
-            className={`pagination-next nav ${nextClassName}`}
-            onClick={() => onChange(currentPage + 1)}
-          >
-            {nextName}
-          </a>
-        </li>
-      </ul>
-    </nav>
-  );
+    if (currentPage <= pages - visibleRadius - 1 && visiblePages.length < pages) {
+        if (currentPage < pages - visibleRadius - 1) {
+            pagesComponents.push(<Ellipses key="ellipses-2" />);
+        }
+
+        pagesComponents.push(
+            <Page key={`page-${pages}`} index={pages} currentPage={currentPage} onChange={onChange} />,
+        );
+    }
+
+    return (
+        <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+            <div className="flex-1 flex justify-between sm:hidden">
+                <a
+                    href="#"
+                    className="relative inline-flex items-center px-4 py-2 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+                >
+                    Previous
+                </a>
+                <a
+                    href="#"
+                    className="ml-3 relative inline-flex items-center px-4 py-2 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+                >
+                    Next
+                </a>
+            </div>
+            <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+                <nav
+                    className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
+                    role="navigation"
+                    aria-label="pagination"
+                >
+                    <div className="relative inline-flex px-4 ">
+                        {/* eslint-disable-next-line radix */}
+                        <select
+                            defaultValue="10"
+                            className="mt-1 block w-full py-2 px-3 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                            onChange={(e) => onPerPageChange(parseInt(e.target.value))}
+                        >
+                            <option value="5">5</option>
+                            <option value="10">10</option>
+                            <option value="25">25</option>
+                            <option value="50">50</option>
+                            <option value="100">100</option>
+                            <option value="1000">1000</option>
+                        </select>
+                    </div>
+                    <div hidden={currentPage === 1}>
+                        <a
+                            disabled={currentPage === 1}
+                            className="relative inline-flex items-center px-2 py-2 rounded-l-md bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                            onClick={() => onChange(currentPage - 1)}
+                        >
+                            <span className="sr-only">Previous</span>
+                            <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+                        </a>
+                    </div>
+                    {pagesComponents}
+                    <div hidden={currentPage === pages}>
+                        <a
+                            disabled={currentPage === pages}
+                            className="relative inline-flex items-center px-2 py-2 rounded-r-md bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                            onClick={() => onChange(currentPage + 1)}
+                        >
+                            <span className="sr-only">Next</span>
+                            <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+                        </a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    );
 };
 
 Pagination.propTypes = {
-  pages: PropTypes.number.isRequired,
-  currentPage: PropTypes.number,
-  visibleRadius: PropTypes.number,
-  className: PropTypes.string,
-  prevName: PropTypes.string,
-  nextName: PropTypes.string,
-  prevClassName: PropTypes.string,
-  nextClassName: PropTypes.string,
-  linkClassName: PropTypes.string,
-  listClassName: PropTypes.string,
-  onChange: PropTypes.func.isRequired,
-  onPerPageChange: PropTypes.func.isRequired,
-  isRounded: PropTypes.bool,
-  isSmall: PropTypes.bool,
-  isMedium: PropTypes.bool,
-  isLarge: PropTypes.bool,
-  isCentered: PropTypes.bool,
-  isRight: PropTypes.bool,
+    pages: PropTypes.number.isRequired,
+    currentPage: PropTypes.number,
+    visibleRadius: PropTypes.number,
+    className: PropTypes.string,
+    prevName: PropTypes.string,
+    nextName: PropTypes.string,
+    prevClassName: PropTypes.string,
+    nextClassName: PropTypes.string,
+
+    listClassName: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    onPerPageChange: PropTypes.func.isRequired,
+    isRounded: PropTypes.bool,
+    isSmall: PropTypes.bool,
+    isMedium: PropTypes.bool,
+    isLarge: PropTypes.bool,
+    isCentered: PropTypes.bool,
+    isRight: PropTypes.bool,
 };
 
 Pagination.defaultProps = {
-  currentPage: 1,
-  visibleRadius: 1,
-  className: '',
-  prevName: '<<',
-  nextName: '>>',
-  prevClassName: '',
-  nextClassName: '',
-  linkClassName: '',
-  listClassName: '',
-  isRounded: false,
-  isSmall: false,
-  isMedium: false,
-  isLarge: false,
-  isCentered: true,
-  isRight: false,
+    currentPage: 1,
+    visibleRadius: 1,
+    className: '',
+    prevName: '<<',
+    nextName: '>>',
 };
 
 export default Pagination;
