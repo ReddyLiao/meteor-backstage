cb1676c0bb1c52622a989f0091e5cafd64215bef
Rumor <rumor.liao@veden.dental>
2021-12-24 13:40:27 +0800

    add chang password modal

--- imports/ui/component/NavBarEnd.js
+++ imports/ui/component/NavBarEnd.js
@@ -1,9 +1,9 @@
 import { Accounts } from 'meteor/accounts-base';
-import React, { useState, useContext, Fragment } from 'react';
+import React, { useState, useContext, Fragment, useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import { RoutingContext } from '/imports/ui/route';
-import { Menu, Transition } from '@headlessui/react';
+import { Menu, Dialog, Transition } from '@headlessui/react';
 
 const NavBarEnd = () => {
     const history = useHistory();
@@ -13,6 +13,8 @@ const NavBarEnd = () => {
         newPassword: '',
         error: '',
     });
+    const [open, setOpen] = useState(false);
+    const cancelButtonRef = useRef(null);
 
     function logout() {
         Accounts.logout();
@@ -20,8 +22,7 @@ const NavBarEnd = () => {
     }
 
     function showPasswordModal() {
-        document.getElementById('password-modal').classList.toggle('is-active');
-        document.getElementsByTagName('html')[0].classList.toggle('is-clipped');
+        setOpen(true);
     }
     function classNames(...classes) {
         return classes.filter(Boolean).join(' ');
@@ -39,8 +40,12 @@ const NavBarEnd = () => {
                     setPassword(password);
                 } else {
                     password.error = '';
-                    document.getElementById('password-modal').classList.toggle('is-active');
-                    document.getElementsByTagName('html')[0].classList.toggle('is-clipped');
+                    Swal.fire({
+                        icon: 'success',
+                        title: '成功',
+                        text: '密碼已更新',
+                    }).then();
+                    setOpen(false);
                 }
             });
         } else {
@@ -48,16 +53,13 @@ const NavBarEnd = () => {
                 icon: 'error',
                 title: '錯誤',
                 text: '請輸入舊密碼及新密碼.',
-
-                // eslint-disable-next-line no-unused-vars
             }).then((r) => {});
             setPassword(password);
         }
     }
 
     function handleClose() {
-        document.getElementById('password-modal').classList.toggle('is-active');
-        document.getElementsByTagName('html')[0].classList.toggle('is-clipped');
+        setOpen(false);
     }
 
     const handleChange = (e) => {
@@ -123,74 +125,109 @@ const NavBarEnd = () => {
                     </Menu.Item>
                 </Menu.Items>
             </Transition>
-            {/* <div className="modal" id="password-modal">
-                <div className="modal-background" />
-                <div className="modal-card">
-                    <header className="modal-card-head">
-                        <p className="modal-card-title">更改密碼</p>
-                    </header>
-                    <section className="modal-card-body has-text-black">
-                        <form className="box">
-                            <div className="field">
-                                <label className="label">帳號</label>
-                                <div className="control has-text-grey">
-                                    <input
-                                        className="input has-text-grey"
-                                        type="text"
-                                        name="username"
-                                        readOnly={true}
-                                        autoComplete="off"
-                                        value={user?.username || ''}
-                                    />
-                                </div>
-                            </div>
-                            <div className="field">
-                                <label className="label">舊密碼</label>
-                                <div className="control has-icons-left">
-                                    <input
-                                        className="input"
-                                        type="password"
-                                        name="oldPassword"
-                                        placeholder="********"
-                                        autoComplete="on"
-                                        required
-                                        onChange={handleChange}
-                                    />
-                                    <span className="icon is-small is-left">
-                                        <i className="fa fa-lock" />
-                                    </span>
-                                </div>
-                            </div>
+            <Transition.Root show={open} as={Fragment}>
+                <Dialog
+                    as="div"
+                    className="fixed z-10 inset-0 overflow-y-auto"
+                    initialFocus={cancelButtonRef}
+                    onClose={setOpen}
+                >
+                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                        </Transition.Child>
+                        {/* This element is to trick the browser into centering the modal contents. */}
+                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+                            &#8203;
+                        </span>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enterTo="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                    <div className="sm:flex sm:items-start">
+                                        <div className="px-4 py-5 bg-white sm:p-6">
+                                            <h3 className="text-lg font-medium leading-6 text-gray-900">更改密碼</h3>
+                                            <div className="grid grid-cols-6 gap-6">
+                                                <div className="col-span-6 sm:col-span-4">
+                                                    <label className="block text-sm font-medium text-gray-700">
+                                                        帳號
+                                                    </label>
+                                                    <input
+                                                        className="mt-1 bg-gray-100  block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                                        type="text"
+                                                        name="username"
+                                                        readOnly={true}
+                                                        autoComplete="off"
+                                                        value={user?.username || ''}
+                                                    />
+                                                </div>
+                                                <div className="col-span-6 sm:col-span-3">
+                                                    <label className="block text-sm font-medium text-gray-700">
+                                                        舊密碼
+                                                    </label>
+                                                    <input
+                                                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                                        type="password"
+                                                        name="oldPassword"
+                                                        placeholder="********"
+                                                        autoComplete="on"
+                                                        required
+                                                        onChange={handleChange}
+                                                    />
+                                                </div>
 
-                            <div className="field">
-                                <label className="label">新密碼</label>
-                                <div className="control has-icons-left">
-                                    <input
-                                        className="input"
-                                        type="password"
-                                        name="newPassword"
-                                        autoComplete="on"
-                                        placeholder="********"
-                                        required
-                                        onChange={handleChange}
-                                    />
-                                    <span className="icon is-small is-left">
-                                        <i className="fa fa-lock" />
-                                    </span>
+                                                <div className="col-span-6 sm:col-span-3">
+                                                    <label className="block text-sm font-medium text-gray-700">
+                                                        新密碼
+                                                    </label>
+                                                    <input
+                                                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                                        type="password"
+                                                        name="newPassword"
+                                                        autoComplete="on"
+                                                        placeholder="********"
+                                                        required
+                                                        onChange={handleChange}
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                                    <button
+                                        className="mx-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                        onClick={() => handleSave()}
+                                    >
+                                        確認
+                                    </button>
+                                    <button
+                                        className="bg-white mx-2 py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                        onClick={handleClose}
+                                    >
+                                        取消
+                                    </button>
                                 </div>
                             </div>
-                        </form>
-                    </section>
-                    <footer className="modal-card-foot">
-                        <button className="button is-success" onClick={handleSave}>
-                            確認
-                        </button>
-                        <button className="button" data-bulma-modal="close" onClick={handleClose}>
-                            取消
-                        </button>
-                    </footer>
-                </div>
-            </div> */}
+                        </Transition.Child>
+                    </div>
+                </Dialog>
+            </Transition.Root>
         </Menu>
     );
 };
